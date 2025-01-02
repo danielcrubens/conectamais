cb3114c11a298d61a65e112d7b16f2259fa76c1e
danielcrubens <dcr.tecnologi@gmail.com>
2025-01-02 20:19:27 -0300

    style: novas cores

--- components/ServicesStep.vue
+++ components/ServicesStep.vue
@@ -1,4 +1,3 @@
-<!-- components/steps/ServicesStep.vue -->
 <template>
   <div class="space-y-6 py-6 px-2.5">
     <h1 class="text-2xl font-bold py-4 text-neutral-600">
@@ -8,12 +7,12 @@
     <div class="grid gap-4">
       <div v-for="service in services" :key="service.id"
         class="p-4 border-2 rounded-xl cursor-pointer transition-all duration-200" :class="{
-          'border-blue-500 bg-blue-50': selectedServices.includes(service.id),
-          'border-zinc-300 hover:border-blue-200': !selectedServices.includes(service.id)
+          'border-indigo-500 bg-indigo-50': selectedServices.includes(service.id),
+          'border-zinc-300 hover:border-indigo-200': !selectedServices.includes(service.id)
         }" @click="toggleService(service.id)" role="button" :aria-selected="selectedServices.includes(service.id)">
         <div class="flex items-start space-x-4">
           <div class="p-2 rounded-lg" :class="{
-            'bg-blue-500 text-white': selectedServices.includes(service.id),
+            'bg-indigo-500 text-white': selectedServices.includes(service.id),
             'bg-gray-100 text-neutral-600': !selectedServices.includes(service.id)
           }">
 
@@ -22,7 +21,7 @@
             <Palette v-if="service.id === 'uxui'" class="w-6 h-6" />
           </div>
           <div>
-            <h4 class="font-medium text-gray-800">
+            <h4 class="font-medium text-neutral-700">
               {{ service.title }}
             </h4>
             <p class="text-gray-500 text-sm">
@@ -33,26 +32,27 @@
       </div>
     </div>
 
-    <div class="space-x-2 flex py-8">
+    <div class="md:space-x-16 space-x-6 flex py-8 relative">
       <Button :variant="'secondary'" @click="emit('previous')" :disabled="isSubmitting">
+        <MoveLeft :size="25" />
         Voltar
       </Button>
       <Button @click="handleSubmit" :disabled="isSubmitting || selectedServices.length === 0"
         class=" disabled:opacity-50">
         {{ isSubmitting ? 'Enviando...' : 'Finalizar ' }}
+        <Check :size="25" />
       </Button>
-
-    </div>
-
-    <p v-if="errorMessage" class="text-red-500 text-sm text-center mt-2">
+      <p v-if="errorMessage" class="text-red-500 text-sm text-center absolute -bottom-3.5 left-0 right-0">
       {{ errorMessage }}
     </p>
+    </div>
+  
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Props, Service } from '~/types'
-import { Monitor, Smartphone, Palette } from 'lucide-vue-next';
+import { Monitor, Smartphone, Palette, MoveLeft, Check  } from 'lucide-vue-next';
 import Button from '@/components/Buttons/Button.vue';
 
 
@@ -100,7 +100,7 @@ const handleSubmit = async () => {
   }
 
   if (!props.userData?.email || !props.userData?.name) {
-    errorMessage.value = 'Dados do usuário incompletos'
+    errorMessage.value = 'Dados básicos incompletos'
     return
   }
 
