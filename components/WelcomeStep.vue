8972cbd891b66dfe60adfb82cc78f7abf3bb4e52
danielcrubens <dcr.tecnologi@gmail.com>
2025-01-02 20:21:04 -0300

    feat: adicionando imagem

--- components/WelcomeStep.vue
+++ components/WelcomeStep.vue
@@ -1,14 +1,15 @@
 <template>
   <div class=" flex items-center justify-center">
     <div class=" shadow-xl overflow-hidden">
-      <div class=" p-8 text-neutral-600 text-center">
-        <h1 class="text-2xl font-bold mb-4">
+      <div class=" py-10 px-2.5 text-neutral-600 text-center">
+        <h1 class="text-2xl font-bold">
           Mapeamento de Necessidades
         </h1>
-        <p class=" text-lg mb-10">
+        <p class=" text-lg py-5">
           Entenda suas necessidades digitais e receba soluções personalizadas para impulsionar seu negócio
         </p>
-        <Button variant="primary" @click="emit('next')">
+        <img class=" w-72 h-auto mx-auto py-10" src="@/assets/images/map.svg" alt="mapeamento de necessidades" />
+        <Button  size="lg" variant="primary" @click="emit('next')">
           Começar Jornada
           <MoveRight :size="25" />
         </Button>
