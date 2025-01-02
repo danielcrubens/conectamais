be40d12d39436638db052606f8bd82fcaca75e62
danielcrubens <dcr.tecnologi@gmail.com>
2025-01-02 20:15:15 -0300

    feat: criando nova variant size

--- components/Buttons/Button.vue
+++ components/Buttons/Button.vue
@@ -1,7 +1,8 @@
 <template>
   <button type="button" :class="{
-    'flex justify-center items-center gap-3 w-full text-lg font-semibold py-2.5 rounded-md': true,
+    'flex justify-center items-center gap-3 lg:text-lg font-semibold py-2.5 rounded-md': true,
     ...VariantClass,
+    ...SizeClass,
   }" v-bind="$attrs">
     <slot />
   </button>
@@ -18,13 +19,22 @@ const props = defineProps({
       return ['primary', 'secondary'].includes(value);
     },
   },
- 
+ size : {
+    type: String,
+    default: 'default',
+    validator(value) {
+      return ['default','lg'].includes(value);
+    },
+  },
 });
 
 const VariantClass = computed(() => ({
-  'bg-blue text-white hover:brightness-90': props.variant === 'primary',
-  'bg-white text-blue': props.variant === 'secondary',
+  'bg-indigo-600 text-white hover:brightness-90': props.variant === 'primary',
+  'bg-white text-indigo-600 border border-indigo-600': props.variant === 'secondary',
 }));
-
+ const SizeClass = computed(() => ({
+    'w-full': props.size === 'default',
+    'md:w-6/12 mx-auto w-full': props.size === 'lg',
+  }));
 
 </script>
